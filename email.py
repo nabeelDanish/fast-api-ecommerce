22b7b379567941b1648b6a1c50534f7476a2707f
nabeelDanish <nabeelben@gmail.com>
2023-06-21 14:46:49 +0400

    Email module added

--- email.py
+++ email.py
@@ -1,3 +1,5 @@
+import jwt
+
 from fastapi import (
     BackgroundTasks,
     UploadFile,
@@ -8,13 +10,62 @@ from fastapi import (
     status,
 )
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from dotenv import dotenv_values
+from pydantic import BaseModel, EmailStr
+from typing import List
+
+from .models import User
+
+credentials = dotenv_values(".env")
 
 config = ConnectionConfig(
-    MAIL_USERNAME="",
-    MAIL_PASSWORD="",
-    MAIL_FROM="",
+    MAIL_USERNAME=credentials["EMAIL"],
+    MAIL_PASSWORD=["PASSWORD"],
+    MAIL_FROM=["EMAIL"],
     MAIL_PORT=587,
     MAIL_SERVER="smpt.gmail.com",
     MAIL_SSL_TLS=True,
     USE_CREDENTIALS=True,
 )
+
+
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+
+
+async def send_email(email: EmailSchema, instance: User):
+    token_data = {
+        "id": instance.id,
+        "username": instance.username,
+    }
+
+    token = jwt.encode(token_data, credentials["SECRET"])
+
+    template = f"""
+        <!DOCTYPE html>
+        <html>
+            <head></head>
+            <body>
+                <div style="display: flex; align-items: center; justify-content: center; flex-direction: column">
+                    <h3>Account Verification</h3>
+                    <br>
+                    <p>Thanks for choosing Noon, please click on the button below to verify your account</p>
+                    <a style="margin-top: 1rem; padding: 1rem; border-radius: 0.5rem; font-size: 1rem; text-decoration: none; background: #0275d8; color: white;" href="http://localhost:8000/verification/?token={token}">
+                        Verify your email
+                    </a>
+                    <br>
+                    <p>Please kindly ignore this email if you did not register for Noon and nothing will happen. Thanks!</p>
+                </div>
+            </body>
+        </html>
+    """
+
+    message = MessageSchema(
+        subject="Noon Account Verification Email",
+        recipients=email,
+        body=template,
+        subtype="html",
+    )
+
+    fm = FastMail(config)
+    await fm.send_message(message=message)
